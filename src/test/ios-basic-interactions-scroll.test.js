6e5b2e3c08a06483d8b7cc931928324554c1e2cd
amy393 <guqing0909@gmail.com>
2022-05-16 23:06:47 +0800

    use touchAction scroll element ^0^

--- src/test/ios-basic-interactions-scroll.test.js
+++ src/test/ios-basic-interactions-scroll.test.js
@@ -18,6 +18,30 @@ describe('Basic IOS interactions', function () {
 
    it('should scroll a button', async function () {
     const element = await client.findElement('xpath', '//XCUIElementTypeSlider[@name="AppElem"]');
-    await client.touchScroll(100,0,element.ELEMENT);
-  });
-});
+    console.log('scrollElement is : ',element)
+    // await client.touchScroll(100,0,element.ELEMENT); 控件没有实现这个方法
+    // browser.touchDown(x, y)
+    let rect = await client.getElementAttribute(element.ELEMENT, 'rect');
+    // console.log('rect is : ',rect.x,' type is ',typeof rect)
+    let rectJson = JSON.parse(rect);
+    // console.log('rectJson is : ',rectJson.x,' type is ',typeof rectJson.x)
+    let startX = rectJson.x + (rectJson.width/2);
+    let startY = rectJson.y + (rectJson.height/2);
+    let desX = startX+(rectJson.width/4*1);
+    let desY = startY;
+    // console.log('desX is : ',desX,' desY is : ',desY," type is: ",typeof desX)
+    // await client.touchDown(rectJson.x,rectJson.y);
+    // // browser.touchMove(x, y)
+    // await client.touchMove(desX,desY);
+    // // browser.touchUp(x, y)
+    // await client.touchUp(desX,desY);
+    let touchActionParams = [
+      { action: 'press', x: startX, y: startY },
+      { action: 'moveTo', x: desX, y: desY },
+      { action:'wait', ms: 1000 },
+      'release'
+     ]
+    // console.log('touchActionParams is : ',touchActionParams)
+    await client.touchAction(touchActionParams);
+    })
+})
